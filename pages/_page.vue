05e6d49fa2b88f673bef39dff61ed65e5e395aea
Andrew Rowland <andrew.rowland@kinandcarta.com>
2021-02-15 14:28:39 +0000

    chore: test repo

---
+++ pages/_page.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ pageContent.title }}</h1>
+    <a href="/test">Now refresh the page</a> - Will see an error in the console
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    // Real data would be obtained from CMS
+    return {
+      pageContent: {
+        title: 'Via generic route to _page',
+      },
+    }
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ pageContent.title }}</h1>
+    <nuxt-link to="/test">Now try via generic route</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData() {
+    // Real data would be obtained from CMS
+    return {
+      pageContent: {
+        title: 'Via index file route',
+      },
+    }
+  },
+}
+</script>
